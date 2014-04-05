2eb493d4666e2ae32f0af80da9c85b1d742fd8a1
Paulo Pires <taralloqueiroiz@gmail.com>
2014-04-04 23:17:35 -0300

    added a app

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,21 @@
 (function(){
   var App = Ember.Application.create();
+
+  DS.Store.create({
+    revision: 12,
+    adapter: DS.RESTAdapter.create({
+      namespace: 'api'
+    })
+  });
+
+  App.Job = DS.Model.extend({
+    name: DS.attr('string'),
+    picture: DS.attr('string')
+  });
+
+  App.IndexRoute = Ember.Route.extend({
+    model: function(){
+      return App.Job.find();
+    }
+  });
 })();
--- server.go
+++ server.go
@@ -2,9 +2,22 @@ package main
 
 import "net/http"
 import "log"
+import "github.com/gorilla/mux"
+
+func JobsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(`{"kittens": [
+    {"id": 1, "name": "Bobby", "picture": "http://placekitten.com/200/200"},
+    {"id": 2, "name": "Wally", "picture": "http://placekitten.com/200/200"}
+  ]}`))
+}
 
 func main() {
 	log.Println("Starting server")
+
+	r := mux.NewRouter()
+	r.HandleFunc("/api/jobs", JobsHandler).Methods("GET")
+
 	http.Handle("/", http.FileServer(http.Dir("./public/")))
 
 	log.Println("Listening on 8080")
