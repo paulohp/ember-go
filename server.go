162407c4a7e26b9bf7577eb7f536994d0454d1fd
Paulo Pires <taralloqueiroiz@gmail.com>
2014-05-04 12:59:21 -0300

    lol

--- server.go
+++ server.go
@@ -12,12 +12,19 @@ func JobsHandler(w http.ResponseWriter, r *http.Request) {
   ]}`))
 }
 
+func PicsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	resp := http.Get("https://api.instagram.com/v1/tags/snow/media/recent?client_id=48b8d07d69a64138bde5a4278e146f91")
+	log.Println(resp)
+}
+
 func main() {
 	log.Println("Starting server")
 
 	r := mux.NewRouter()
 	r.HandleFunc("/api/jobs", JobsHandler).Methods("GET")
 	http.Handle("/api/", r)
+	r.HandleFunc("/api/instagram", PicsHandler)
 
 	http.Handle("/", http.FileServer(http.Dir("./public/")))
 
